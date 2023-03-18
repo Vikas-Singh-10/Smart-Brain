de1d028b0e7aff365416f0fc074430b15f585554
Vikas Singh <vikaskuntal10@gmail.com>
2023-03-18 23:58:07 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(<App />)
 
-// If using React version lower than 18:
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+
